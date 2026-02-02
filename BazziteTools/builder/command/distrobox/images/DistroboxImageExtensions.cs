fadb6ca658343f72e8f0e4b4c4d50fc6af06a47c
andreas <Andreas.Zwander@gmail.com>
2026-02-02 01:14:18 +0100

    Add DistroboxImageExtensions class with ToImageString method to BazziteTools

---
+++ BazziteTools/builder/command/distrobox/images/DistroboxImageExtensions.cs
@@ -0,0 +1,14 @@
+namespace BazziteTools.builder.command.distrobox;
+
+public static class DistroboxImageExtensions
+{
+    public static string ToImageString(this DistroboxImage image) => image switch
+    {
+        DistroboxImage.Ubuntu => "ubuntu:latest",
+        DistroboxImage.Fedora => "fedora:latest",
+        DistroboxImage.Arch   => "archlinux:latest",
+        DistroboxImage.Alpine => "alpine:latest",
+        DistroboxImage.Debian => "debian:stable",
+        _ => throw new ArgumentOutOfRangeException(nameof(image), image, null)
+    };
+}
