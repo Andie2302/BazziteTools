3fe88f26f7046426603872a777bf610060fe645d
andreas <Andreas.Zwander@gmail.com>
2026-02-02 15:48:09 +0100

    Add `IsValid` method to `GenericCommandBuilder` for basic command validation

--- BazziteTools/builder/command/base/GenericCommandBuilder.cs
+++ BazziteTools/builder/command/base/GenericCommandBuilder.cs
@@ -2,4 +2,9 @@ namespace BazziteTools.builder.command.@base;
 
 public class GenericCommandBuilder(string binary) : LinuxCommandBuilder<GenericCommandBuilder>(binary)
 {
+    public override bool IsValid(out List<string> errors)
+    {
+        errors = [];
+        return true;
+    }
 }
