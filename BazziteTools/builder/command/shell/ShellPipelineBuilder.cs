402cb25b1be4b01efecbc2714e5dce7038f83466
andreas <Andreas.Zwander@gmail.com>
2026-02-02 14:43:43 +0100

    Add `IsValid` method to `ShellPipelineBuilder` for pipeline command validation

--- BazziteTools/builder/command/shell/ShellPipelineBuilder.cs
+++ BazziteTools/builder/command/shell/ShellPipelineBuilder.cs
@@ -13,4 +13,16 @@ public class ShellPipelineBuilder : ICommand
     }
 
     public string Build() => _commands.Count == 0 ? string.Empty : string.Join(" | ", _commands.Select(c => c.Build()));
+    public bool IsValid(out List<string> errors)
+    {
+        errors = [];
+        foreach (var command in _commands)
+        {
+            if (!command.IsValid(out var cmdErrors))
+            {
+                errors.AddRange(cmdErrors);
+            }
+        }
+        return errors.Count == 0;
+    }
 }
