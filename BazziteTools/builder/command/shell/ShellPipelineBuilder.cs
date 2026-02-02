d1801a60e1d3899818a3df21dbcaa7dbb7baa0bf
andreas <Andreas.Zwander@gmail.com>
2026-02-02 16:52:25 +0100

    Update `ShellPipelineBuilder` to use `ValidationResult` in `IsValid` for consistency with updated command builder interfaces

--- BazziteTools/builder/command/shell/ShellPipelineBuilder.cs
+++ BazziteTools/builder/command/shell/ShellPipelineBuilder.cs
@@ -1,4 +1,5 @@
 using BazziteTools.builder.command.@base;
+using BazziteTools.builder.command.distrobox;
 
 namespace BazziteTools.builder.command.shell;
 
@@ -13,9 +14,9 @@ public class ShellPipelineBuilder : ICommandBuilder
     }
 
     public string Build() => _commands.Count == 0 ? string.Empty : string.Join(" | ", _commands.Select(c => c.Build()));
-    public bool IsValid(out List<string> errors)
+    public bool IsValid(out ValidationResult validationResult)
     {
-        errors = [];
+        validationResult = new ValidationResult();
         foreach (var command in _commands)
         {
             if (!command.IsValid(out var cmdErrors))
@@ -23,6 +24,6 @@ public class ShellPipelineBuilder : ICommandBuilder
                 errors.AddRange(cmdErrors);
             }
         }
-        return errors.Count == 0;
+        return validationResult.IsSuccess;
     }
 }
