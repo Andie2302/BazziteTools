7d70647999c2937009e687df158a3720e72e372f
andreas <Andreas.Zwander@gmail.com>
2026-02-02 15:55:04 +0100

    Add `IsValid` method to `FlatpakSpawnBuilder` for execution command validation

--- BazziteTools/builder/command/flatpak/FlatpakSpawnBuilder.cs
+++ BazziteTools/builder/command/flatpak/FlatpakSpawnBuilder.cs
@@ -1,6 +1,7 @@
 using BazziteTools.builder.command.@base;
 
 namespace BazziteTools.builder.command.flatpak;
+
 public class FlatpakSpawnBuilder : LinuxCommandBuilder<FlatpakSpawnBuilder>
 {
     public FlatpakSpawnBuilder() : base("flatpak-spawn")
@@ -12,5 +13,13 @@ public class FlatpakSpawnBuilder : LinuxCommandBuilder<FlatpakSpawnBuilder>
     {
         AddRawArgument(command);
         return this;
+    }
+
+    public override bool IsValid(out List<string> errors)
+    {
+        errors = [];
+        if (!_arguments.Any(a => !a.Contains("--host")))
+            errors.Add("Kein Befehl zum Ausführen (Wrap) angegeben.");
+        return errors.Count == 0;
     }
 }
