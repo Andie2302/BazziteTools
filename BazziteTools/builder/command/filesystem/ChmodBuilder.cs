d2bfb4145c1a6dc3bd610c940041b7777b2b6394
andreas <Andreas.Zwander@gmail.com>
2026-02-02 21:15:21 +0100

    Remove `ChownBuilder` class from `ChmodBuilder.cs`

--- BazziteTools/builder/command/filesystem/ChmodBuilder.cs
+++ BazziteTools/builder/command/filesystem/ChmodBuilder.cs
@@ -43,32 +43,3 @@ public class ChmodBuilder : LinuxCommandBuilder<ChmodBuilder>
         return report;
     }
 }
-
-public class ChownBuilder : LinuxCommandBuilder<ChownBuilder>
-{
-    private readonly string _path;
-
-    public ChownBuilder(string path) : base("chown")
-    {
-        _path = path;
-    }
-
-    public ChownBuilder ToUser(string user)
-    {
-        AddArgument(user);
-        return this;
-    }
-
-    public ChownBuilder Recursive() => AddShortOption('R');
-
-    public override string Build() => $"{base.Build()} {_path}";
-
-    public override CommandReport Validate()
-    {
-        var report = new CommandReport();
-        if (string.IsNullOrEmpty(_path)) report.AddError("Kein Pfad für chown angegeben.");
-        if (Arguments.All(a => a.StartsWith("-"))) report.AddError("Kein Benutzer angegeben.");
-        return report;
-    }
-}
-
