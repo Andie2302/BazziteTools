8bce683a45c1d53d62791486d602e23c3463cac7
andreas <Andreas.Zwander@gmail.com>
2026-02-02 01:24:05 +0100

    Update Program.cs to use DistroboxCreateBuilder for creating a configured Distrobox instance

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,3 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using BazziteTools;
+using BazziteTools.builder.command.distrobox;
+
 Console.WriteLine("Hello, World!");
+
+DistroboxCreateBuilder builder = new();
+builder.WithLatestImage(DistroboxImage.Ubuntu);
+builder.UseNvidia();
+builder.WithName("ki");
+var x = builder.Build();
+Console.WriteLine(x);
