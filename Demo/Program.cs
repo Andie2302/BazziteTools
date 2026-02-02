a8b5021f8611fadfe7708cdcea3dfdcf96fd388b
andreas <Andreas.Zwander@gmail.com>
2026-02-02 23:50:10 +0100

    Add Distrobox creation logic in `Program.cs` with configurable options for name, home directory, and image selection

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,2 +1,11 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using BazziteTools.builder.command.distrobox;
+using BazziteTools.builder.command.distrobox.images;
 
+Console.WriteLine("Hello, World!");
+
+var name = "ki";
+var home = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile),"distroboxes",name);
+
+var distrobox = DistroBox.Create().WithName(name).WithHome(home).WithLatestImage(DistroboxImage.Ubuntu).UseNvidia().Build();
+
+Console.WriteLine(distrobox);
