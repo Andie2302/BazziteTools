374b5a19989ee8a196f50eea88b5b8dc01746c01
andreas <Andreas.Zwander@gmail.com>
2026-02-02 16:46:43 +0100

    Rename `DistroboxRemoveBuilder` to `DistroboxRemoveBuilderBuilder` for naming consistency with other builder classes

--- BazziteTools/builder/command/distrobox/DistroboxRemoveBuilderBuilder.cs
+++ BazziteTools/builder/command/distrobox/DistroboxRemoveBuilderBuilder.cs
@@ -1,21 +1,21 @@
 using BazziteTools.builder.command.@base;
 
 namespace BazziteTools.builder.command.distrobox;
-public class DistroboxRemoveBuilder : LinuxCommandBuilder<DistroboxRemoveBuilder>
+public class DistroboxRemoveBuilderBuilder : LinuxCommandBuilderBuilder<DistroboxRemoveBuilderBuilder>
 {
-    public DistroboxRemoveBuilder(string containerName) : base("distrobox")
+    public DistroboxRemoveBuilderBuilder(string containerName) : base("distrobox")
     {
         AddArgument("rm");
         AddArgument(containerName);
     }
 
-    public DistroboxRemoveBuilder Force()
+    public DistroboxRemoveBuilderBuilder Force()
     {
         AddLongOption("force");
         return this;
     }
 
-    public DistroboxRemoveBuilder Root()
+    public DistroboxRemoveBuilderBuilder Root()
     {
         AddLongOption("root");
         return this;
