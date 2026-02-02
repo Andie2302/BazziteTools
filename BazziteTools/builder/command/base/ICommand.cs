71092bbed12f05bd0c81642b541ff6359fed2462
andreas <Andreas.Zwander@gmail.com>
2026-02-02 16:47:24 +0100

    Rename `ICommand` to `ICommandBuilder` and update `IsValid` to use `ValidationResult` instead of a list of errors

--- BazziteTools/builder/command/base/ICommand.cs
+++
@@ -1,9 +0,0 @@
-namespace BazziteTools.builder.command.@base;
-
-public interface ICommand
-{
-    string Build();
-    
-    // Prüft, ob der Befehl ausgeführt werden kann
-    bool IsValid(out List<string> errors);
-}
---
+++ BazziteTools/builder/command/base/ICommandBuilder.cs
@@ -0,0 +1,10 @@
+using BazziteTools.builder.command.distrobox;
+
+namespace BazziteTools.builder.command.@base;
+
+public interface ICommandBuilder
+{
+    string Build();
+
+    public bool IsValid(out ValidationResult validationResult);
+}
