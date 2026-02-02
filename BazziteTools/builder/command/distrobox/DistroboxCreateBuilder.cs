296f487e227f24c9ab70770546719393329993cc
andreas <Andreas.Zwander@gmail.com>
2026-02-02 01:14:09 +0100

    Add DistroboxCreateBuilder class to BazziteTools

---
+++ BazziteTools/builder/command/distrobox/DistroboxCreateBuilder.cs
@@ -0,0 +1,32 @@
+namespace BazziteTools.builder.command.distrobox;
+
+public class DistroboxCreateBuilder : LinuxCommandBuilder
+{
+    public DistroboxCreateBuilder() : base("distrobox") 
+    {
+        AddArgument("create");
+    }
+
+    public DistroboxCreateBuilder WithImage(string image)
+    {
+        AddLongOption("image", image);
+        return this;
+    }
+
+    public DistroboxCreateBuilder WithLatestImage(DistroboxImage image)
+    {
+        return WithImage(image.ToImageString());
+    }
+
+    public DistroboxCreateBuilder UseNvidia()
+    {
+        AddLongOption("nvidia");
+        return this;
+    }
+
+    public DistroboxCreateBuilder WithHome(string path)
+    {
+        AddLongOption("home", path);
+        return this;
+    }
+}
