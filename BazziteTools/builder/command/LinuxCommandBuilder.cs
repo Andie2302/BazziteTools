e9b9099d7008f5dcea1ee39733fadba8e516709e
andreas <Andreas.Zwander@gmail.com>
2026-02-02 01:53:26 +0100

    Make LinuxCommandBuilder methods chainable and enhance option formatting logic

--- BazziteTools/builder/command/LinuxCommandBuilder.cs
+++ BazziteTools/builder/command/LinuxCommandBuilder.cs
@@ -2,38 +2,49 @@ namespace BazziteTools.builder.command;
 
 public class LinuxCommandBuilder(string binary)
 {
-    private readonly List<string> _parts = [];
-
+    private readonly List<string> _arguments = [];
     // Fügt ein einfaches Wort hinzu (Subcommand oder Positional)
-    public void AddArgument(string arg) => _parts.Add(arg);
-
-    // Fügt -f oder -rf hinzu
-    public void AddShortOption(char option, string? value = null) 
+    public LinuxCommandBuilder AddArgument(string arg)
     {
-        _parts.Add($"-{option}");
-        if (value != null) _parts.Add(value);
+        _arguments.Add(arg);
+        return this;
     }
-
+    // Fügt -f oder -rf hinzu
+    public LinuxCommandBuilder AddShortOption(char option, string? value = null) 
+    {
+        _arguments.Add($"-{option}");
+        if (value != null) _arguments.Add(value);
+        return this;
+    }
     // Fügt --long-option hinzu
     // In LinuxCommandBuilder.cs
-    public void AddLongOption(string option, string? value = null, char separator = ' ')
+    public LinuxCommandBuilder AddLongOption(string option, string? value = null, char separator = ' ')
     {
-        if (value != null) 
-        {
-            // Einfaches Quoting für Werte mit Leerzeichen
-            var formattedValue = value.Contains(' ') ? $"\"{value}\"" : value;
-            _parts.Add($"--{option}{separator}{formattedValue}");
-        }
-        else 
-        {
-            _parts.Add($"--{option}");
-        }
+        _arguments.Add(FormatLongOption(option, value, separator));
+        return this;
     }
 
-    public string Build() => $"{binary} {string.Join(" ", _parts)}";
-    
-    public void AddRawArgument(string arg) => _parts.Add(arg);
+    private static string FormatLongOption(string option, string? value, char separator) =>
+        value is null
+            ? $"--{option}"
+            : $"--{option}{separator}{QuoteIfNeeded(value)}";
 
-    public void AddCommandSeparator() => _parts.Add("--");
-    
+    private static string QuoteIfNeeded(string value)
+    {
+        return value.Contains(' ') ? $"\"{value}\"" : value;
+    }
+
+    public string Build() => $"{binary} {string.Join(" ", _arguments)}";
+
+    public LinuxCommandBuilder AddRawArgument(string arg)
+    {
+        _arguments.Add(arg);
+        return this;
+    }
+    public LinuxCommandBuilder AddCommandSeparator()
+    {
+        _arguments.Add("--");
+        return this;
+    }
+
 }
