8658d94f87fdc0f28c24209fb7b52b53a5214aaf
andreas <Andreas.Zwander@gmail.com>
2026-02-02 02:07:05 +0100

    Add DistroboxUpgradeBuilder class with chainable methods

---
+++ BazziteTools/builder/command/distrobox/DistroboxUpgradeBuilder.cs
@@ -0,0 +1,18 @@
+namespace BazziteTools.builder.command.distrobox;
+
+public class DistroboxUpgradeBuilder : LinuxCommandBuilder
+{
+    public DistroboxUpgradeBuilder() : base("distrobox") => AddArgument("upgrade");
+
+    public DistroboxUpgradeBuilder All()
+    {
+        AddLongOption("all");
+        return this;
+    }
+
+    public DistroboxUpgradeBuilder Name(string containerName)
+    {
+        AddArgument(containerName);
+        return this;
+    }
+}
