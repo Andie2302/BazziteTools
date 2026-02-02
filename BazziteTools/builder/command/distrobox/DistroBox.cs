a2127da864b6fbb8f4089c3841b99bb5d089940a
andreas <Andreas.Zwander@gmail.com>
2026-02-02 12:43:05 +0100

    Add Upgrade and Assemble methods to DistroBox

--- BazziteTools/builder/command/distrobox/DistroBox.cs
+++ BazziteTools/builder/command/distrobox/DistroBox.cs
@@ -5,8 +5,7 @@ public static class DistroBox
     public static DistroboxCreateBuilder Create() => new();
     public static DistroboxEnterBuilder Enter(string name) => new(name);
     public static DistroboxRemoveBuilder Rm(string name) => new(name);
-    
-    // Neue Methoden
+
     public static DistroboxUpgradeBuilder Upgrade() => new();
     public static DistroboxAssembleBuilder Assemble() => new();
 }
