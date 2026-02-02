22023bd1d7a50d45303108b651e9116f7ad906cc
andreas <Andreas.Zwander@gmail.com>
2026-02-02 16:47:19 +0100

    Introduce `GenericCommandBuilderBuilder` as an abstract base for Linux command builders

---
+++ BazziteTools/builder/command/base/GenericCommandBuilderBuilder.cs
@@ -0,0 +1,15 @@
+using BazziteTools.builder.command.distrobox;
+
+namespace BazziteTools.builder.command.@base;
+
+public abstract class GenericCommandBuilderBuilder(string binary) : LinuxCommandBuilderBuilder<GenericCommandBuilderBuilder>(binary)
+{
+    
+    /*
+    {
+        validationResult = new ValidationResult();
+        return validationResult.IsSuccess;
+    }
+    //*/
+
+}
