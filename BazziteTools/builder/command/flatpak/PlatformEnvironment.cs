bbf312550053b8820ea6270b3eef62c4372479d9
andreas <Andreas.Zwander@gmail.com>
2026-02-02 14:43:38 +0100

    Add `IsNvidiaDriverLoaded` property to check for NVIDIA driver presence

--- BazziteTools/builder/command/flatpak/PlatformEnvironment.cs
+++ BazziteTools/builder/command/flatpak/PlatformEnvironment.cs
@@ -1,8 +1,12 @@
 namespace BazziteTools.builder.command.flatpak;
 
+
 public static class PlatformEnvironment
 {
-    private static readonly string FlatpakInfoPath = "/.flatpak-info";
+    private const string FlatpakInfoPath = "/.flatpak-info";
+    private const string NvidiaDriverPath = "/proc/driver/nvidia"; // Pfad zu den NVIDIA-Treiber-Infos
 
     public static bool IsFlatpak => File.Exists(FlatpakInfoPath);
+
+    public static bool IsNvidiaDriverLoaded => Directory.Exists(NvidiaDriverPath);
 }
