01ba8a811a2d4ce5a14ce0f01aa6bcbcc0977373
andreas <Andreas.Zwander@gmail.com>
2026-02-02 02:23:04 +0100

    Add FlatpakBuilder class with chainable methods to build flatpak commands

---
+++ BazziteTools/builder/command/flatpak/FlatpakBuilder.cs
@@ -0,0 +1,33 @@
+namespace BazziteTools.builder.command.flatpak;
+
+public class FlatpakBuilder : LinuxCommandBuilder
+{
+    public FlatpakBuilder() : base("flatpak") { }
+
+    public FlatpakBuilder Run(string appId)
+    {
+        AddArgument("run");
+        AddArgument(appId);
+        return this;
+    }
+
+    public FlatpakBuilder Install(string source, string appId)
+    {
+        AddArgument("install");
+        AddArgument(source);
+        AddArgument(appId);
+        return this;
+    }
+
+    public FlatpakBuilder User()
+    {
+        AddLongOption("user");
+        return this;
+    }
+
+    public FlatpakBuilder AssumeYes()
+    {
+        AddShortOption('y');
+        return this;
+    }
+}
