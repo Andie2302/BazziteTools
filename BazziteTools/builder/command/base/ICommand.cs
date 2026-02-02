2fddad81fe35a62018482342d22754e672292f3f
andreas <Andreas.Zwander@gmail.com>
2026-02-02 14:36:53 +0100

    Add `IsValid` method to `ICommand` interface for command validation

--- BazziteTools/builder/command/base/ICommand.cs
+++ BazziteTools/builder/command/base/ICommand.cs
@@ -3,4 +3,7 @@ namespace BazziteTools.builder.command.@base;
 public interface ICommand
 {
     string Build();
+    
+    // Prüft, ob der Befehl ausgeführt werden kann
+    bool IsValid(out List<string> errors);
 }
