e73f0ce58130a54d7fad9e0a646eecee09224ed9
andreas <Andreas.Zwander@gmail.com>
2026-02-02 12:35:49 +0100

    Update FlatpakInstallBuilder to support optional appId parameter

--- BazziteTools/builder/command/flatpak/FlatPak.cs
+++ BazziteTools/builder/command/flatpak/FlatPak.cs
@@ -3,6 +3,17 @@ namespace BazziteTools.builder.command.flatpak;
 public static class FlatPak
 {
     public static FlatpakBuilder Command() => new();
+    
     public static FlatpakSpawnBuilder Spawn() => new();
-    public static FlatpakInstallBuilder Install() => new();
+
+    // Diese Methode hat gefehlt oder hatte den falschen Parameter:
+    public static FlatpakInstallBuilder Install(string? appId = null)
+    {
+        var builder = new FlatpakInstallBuilder();
+        if (appId != null) 
+        {
+            builder.App(appId);
+        }
+        return builder;
+    }
 }
