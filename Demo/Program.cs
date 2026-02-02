2e85d24864156e765e7bbf032e599dcbd9f02115
andreas <Andreas.Zwander@gmail.com>
2026-02-02 02:07:10 +0100

    Update Program.cs to demonstrate full Distrobox lifecycle: create, enter, and remove

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -5,9 +5,24 @@ using BazziteTools.builder.command.distrobox.images;
 
 Console.WriteLine("Hello, World!");
 
-DistroboxCreateBuilder builder = new();
-builder.WithLatestImage(DistroboxImage.Ubuntu);
-builder.UseNvidia();
-builder.WithName("ki");
-var x = builder.Build();
-Console.WriteLine(x);
+
+// 1. Container erstellen (dein bestehendes Beispiel)
+var createCmd = new DistroboxCreateBuilder()
+    .WithLatestImage(DistroboxImage.Ubuntu)
+    .UseNvidia()
+    .WithName("ki")
+    .Build();
+
+// 2. In den Container gehen und einen Befehl ausführen
+var enterCmd = new DistroboxEnterBuilder("ki")
+    .WithCommand("nvidia-smi") // Testet deine RTX 5060 Ti im Container
+    .Build();
+
+// 3. Container löschen
+var rmCmd = new DistroboxRemoveBuilder("ki")
+    .Force()
+    .Build();
+
+Console.WriteLine($"Create: {createCmd}");
+Console.WriteLine($"Enter:  {enterCmd}");
+Console.WriteLine($"Remove: {rmCmd}");
