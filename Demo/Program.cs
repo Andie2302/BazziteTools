620a17dfa31aa85870d6a7d1d9ea6b19d0564e30
andreas <Andreas.Zwander@gmail.com>
2026-02-03 00:17:13 +0100

    Add filesystem setup and command execution logic in `Program.cs`

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,5 +1,7 @@
 ﻿using BazziteTools.builder.command.distrobox;
 using BazziteTools.builder.command.distrobox.images;
+using BazziteTools.builder.command.filesystem;
+using BazziteTools.executor;
 
 Console.WriteLine("Hello, World!");
 
@@ -9,3 +11,7 @@ var home = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.User
 var distrobox = DistroBox.Create().WithName(name).WithHome(home).WithLatestImage(DistroboxImage.Ubuntu).UseNvidia().Build();
 
 Console.WriteLine(distrobox);
+
+var setup = FileSystemWorkflows.PrepareAdminFolder("/tmp/mytool", "username");
+await CommandExecutor.ExecuteAsync(setup);
+
