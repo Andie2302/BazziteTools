1b00b53a427c28cb38cd617498d31c9d84f5c329
andreas <Andreas.Zwander@gmail.com>
2026-02-02 02:06:56 +0100

    Add DistroboxEnterBuilder class with chainable methods

---
+++ BazziteTools/builder/command/distrobox/DistroboxEnterBuilder.cs
@@ -0,0 +1,23 @@
+namespace BazziteTools.builder.command.distrobox;
+
+public class DistroboxEnterBuilder : LinuxCommandBuilder
+{
+    public DistroboxEnterBuilder(string containerName) : base("distrobox")
+    {
+        AddArgument("enter");
+        AddArgument(containerName);
+    }
+
+    public DistroboxEnterBuilder WithCommand(string command)
+    {
+        AddCommandSeparator();
+        AddArgument(command);
+        return this;
+    }
+
+    public DistroboxEnterBuilder Root()
+    {
+        AddLongOption("root");
+        return this;
+    }
+}
