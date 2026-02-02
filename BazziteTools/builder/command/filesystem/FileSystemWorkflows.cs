120ba00bf9c818e9f5e75255a5ef1903b099f628
andreas <Andreas.Zwander@gmail.com>
2026-02-02 21:58:15 +0100

    Add `FileSystemWorkflows` class with `PrepareAdminFolder` method placeholder

---
+++ BazziteTools/builder/command/filesystem/FileSystemWorkflows.cs
@@ -0,0 +1,9 @@
+using BazziteTools.builder.command.@base;
+
+public class FileSystemWorkflows
+{
+    public static ICommandBuilder PrepareAdminFolder(string tmpKiData, string andreas)
+    {
+        throw new NotImplementedException();
+    }
+}
