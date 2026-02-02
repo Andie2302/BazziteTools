7a04d0f78237a5316c1cf9f8ca3f63eceb75ab8e
andreas <Andreas.Zwander@gmail.com>
2026-02-03 00:17:18 +0100

    Refactor `FileSystemWorkflows` to use static methods with detailed documentation and implement folder creation logic with root permissions

--- BazziteTools/builder/command/filesystem/FileSystemWorkflows.cs
+++ BazziteTools/builder/command/filesystem/FileSystemWorkflows.cs
@@ -1,9 +1,24 @@
-using BazziteTools.builder.command.@base;
+namespace BazziteTools.builder.command.filesystem;
 
-public class FileSystemWorkflows
+using @base;
+using shell;
+
+public static class FileSystemWorkflows
 {
-    public static ICommandBuilder PrepareAdminFolder(string tmpKiData, string andreas)
+    /// <summary>
+    /// Erstellt einen Ordner mit Root-Rechten und übergibt danach den Besitz an einen spezifischen Benutzer.
+    /// </summary>
+    /// <param name="path">Der Pfad, der erstellt werden soll.</param>
+    /// <param name="username">Der Benutzer, der Besitzer des Ordners werden soll.</param>
+    /// <returns>Ein kombinierter Befehl (Pipeline).</returns>
+    public static ICommandBuilder PrepareAdminFolder(string path, string username)
     {
-        throw new NotImplementedException();
+        return new ShellPipelineBuilder()
+            .Pipe(FileSystem.CreateDirectory(path)
+                .CreateParents()
+                .AsRoot()) // sudo mkdir -p ...
+            .Pipe(new ChownBuilder(path)
+                .ToUser(username)
+                .AsRoot()); // sudo chown ...
     }
 }
