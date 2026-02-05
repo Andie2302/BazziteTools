387480370da3e97c340d09eb43e2aee577162704
andreas <Andreas.Zwander@gmail.com>
2026-02-05 22:29:01 +0100

    Update `CommandBuilder` to use `ICommandParameter` interface for added flexibility

--- BazziteTools/CommandBuilder.cs
+++ BazziteTools/CommandBuilder.cs
@@ -6,20 +6,20 @@ public class CommandBuilder
 {
     private readonly StringBuilder _commandBuilder = new();
 
-    public CommandBuilder(string executable, params CommandParameter[] parameters)
+    public CommandBuilder(string executable, params ICommandParameter[] parameters)
     {
         _commandBuilder.Append(executable);
         AddRange(parameters);
     }
 
-    public CommandBuilder Add(CommandParameter parameter)
+    public CommandBuilder Add(ICommandParameter parameter)
     {
         _commandBuilder.Append(' ');
         _commandBuilder.Append(parameter);
         return this;
     }
 
-    public CommandBuilder AddRange(params CommandParameter[] parameter)
+    public CommandBuilder AddRange(params ICommandParameter[] parameter)
     {
         foreach (var commandParameter in parameter)
             Add(commandParameter);
