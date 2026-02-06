bc9af46ff3fe8d9bd8d14ea733c11639d9bf4545
andreas <Andreas.Zwander@gmail.com>
2026-02-06 18:27:38 +0100

    Remove outdated test command examples and unused variables from `Program.cs` for cleaner implementation

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,13 +1,12 @@
 ﻿
 
 using BazziteTools.builder.command.@base;
+using BazziteTools.builder.command.@base.@static;
 using BazziteTools.builder.command.distrobox;
 using BazziteTools.executor;
 
 Console.WriteLine("=== BazziteTools Integration Test ===\n");
 
-// 1. Test: Ein klassischer Distrobox-Befehl
-// Erwartet: distrobox create --name development --nvidia --root
 var distrobox = new Command("distrobox")
     .Add(P.Verb("create"))
     .Add(P.LOpt("name", "development"))
@@ -19,9 +18,6 @@ Console.WriteLine(distrobox.Build());
 Console.WriteLine();
 
 
-// 2. Test: Hardware-Abfrage deiner NVIDIA RTX 5060 Ti
-// Erwartet: nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader
-// Hier nutzen wir .WithSeparator("="), falls nvidia-smi das benötigt
 var gpuCheck = new Command("nvidia-smi")
     .Add(P.LOpt("query-gpu", "temperature.gpu").WithSeparator("="))
     .Add(P.LOpt("format", "csv,noheader").WithSeparator("="));
@@ -31,8 +27,6 @@ Console.WriteLine(gpuCheck.Build());
 Console.WriteLine();
 
 
-// 3. Test: System-nahe Zuweisungen (wie dd oder Pfade)
-// Erwartet: sudo dd if=/dev/nvme0n1 of=/home/andreas/backup.img bs=4M
 var ddBackup = new Command("sudo dd")
     .Add(P.Assign("if", "/dev/nvme0n1"))
     .Add(P.Assign("of", "/home/andreas/backup.img"))
@@ -43,8 +37,6 @@ Console.WriteLine(ddBackup.Build());
 Console.WriteLine();
 
 
-// 4. Test: Flatpak mit speziellen Suffixen (Fluent-Check)
-// Erwartet: flatpak install --user! flathub
 var flatpak = new Command("flatpak")
     .Add(P.Verb("install"))
     .Add(P.LFlag("user").WithSuffix("!"))
@@ -60,18 +52,15 @@ var assemble = new DistroboxAssembleCommand()
     .Replace();
 
 Console.WriteLine(assemble.Build());
-// Erwartet: distrobox assemble create --file ./distrobox.ini --replace
 
 var executor = new CommandExecutor();
 
-// Wir bauen den Befehl für NVIDIA
 var gpuTempCmd = new Command("nvidia-smi")
     .Add(P.LOpt("query-gpu", "temperature.gpu").WithSeparator("="))
     .Add(P.LOpt("format", "csv,noheader,nounits").WithSeparator("="));
 
 Console.WriteLine($"Führe aus: {gpuTempCmd.Build()}");
 
-// Ausführung (asynchron)
 string result = await executor.ExecuteAsync(gpuTempCmd);
 
 Console.WriteLine($"Aktuelle GPU Temperatur: {result}°C");
