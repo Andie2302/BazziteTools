571ed9dfd0fcdcd1c3415658b4eeb104409690d2
andreas <Andreas.Zwander@gmail.com>
2026-02-06 15:22:51 +0100

    Add `KeyValueParameter` class to define key-value parameter behavior in `BazziteTools.builder.command.@base` namespace

---
+++ BazziteTools/builder/command/base/KeyValueParameter.cs
@@ -0,0 +1,38 @@
+namespace BazziteTools.builder.command.@base;
+
+public class KeyValueParameter : KeyParameter, IKeyValueParameter<KeyValueParameter, KeyValueParameter>
+{
+    public string Value { get; set; } = string.Empty;
+    public string Separator { get; set; } = "=";
+
+    public KeyValueParameter WithValue(string value)
+    {
+        Value = value;
+        return this;
+    }
+
+    public KeyValueParameter WithSeparator(string separator)
+    {
+        Separator = separator;
+        return this;
+    }
+
+    public new KeyValueParameter WithPrefix(string prefix)
+    {
+        base.WithPrefix(prefix);
+        return this;
+    }
+
+    public new KeyValueParameter WithSuffix(string suffix)
+    {
+        base.WithSuffix(suffix);
+        return this;
+    }
+
+    public new KeyValueParameter WithKey(string key)
+    {
+        base.WithKey(key);
+        return this;
+    }
+    public override string Build() => $"{base.Build()}{Separator}{Value}";
+}
