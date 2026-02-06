9cf48e9cafc0b4875cad6a5e24c7bfa2cb8effd7
andreas <Andreas.Zwander@gmail.com>
2026-02-06 18:26:25 +0100

    Move `ArgumentParameter` class to `BazziteTools.builder.command.@base.special` namespace

--- BazziteTools/builder/command/base/special/ArgumentParameter.cs
+++ BazziteTools/builder/command/base/special/ArgumentParameter.cs
@@ -1,3 +1,3 @@
-namespace BazziteTools.builder.command.@base;
+namespace BazziteTools.builder.command.@base.special;
 
 public class ArgumentParameter : KeyParameter { public ArgumentParameter(string val) => Key = val; }
