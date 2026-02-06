655d8f7bdfc7e310678cd1edd10da7d9fbbaef56
andreas <Andreas.Zwander@gmail.com>
2026-02-06 18:27:42 +0100

    Move `ShortFlagParameter` to `BazziteTools.builder.command.@base.special` namespace and add dependency on `@base.enums`

--- BazziteTools/builder/command/base/special/ShortFlagParameter.cs
+++ BazziteTools/builder/command/base/special/ShortFlagParameter.cs
@@ -1,3 +1,5 @@
-namespace BazziteTools.builder.command.@base;
+using BazziteTools.builder.command.@base.enums;
+
+namespace BazziteTools.builder.command.@base.special;
 
 public class ShortFlagParameter : KeyParameter { public ShortFlagParameter() => Prefix = Prefixes.Dash.ToValue(); }
