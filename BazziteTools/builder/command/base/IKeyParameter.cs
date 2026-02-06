eceeb444da03cf70d5b92c48171c77a7fe23aa10
andreas <Andreas.Zwander@gmail.com>
2026-02-06 15:22:35 +0100

    Add `IKeyParameter` interface to define key-based command parameter behavior in `BazziteTools.builder.command.@base` namespace

---
+++ BazziteTools/builder/command/base/IKeyParameter.cs
@@ -0,0 +1,11 @@
+namespace BazziteTools.builder.command.@base;
+
+public interface IKeyParameter<out TKey> : ICommandParameter
+{
+    string Key { get; set; }
+    string Prefix { get; set; }
+    string Suffix { get; set; }
+    public TKey WithPrefix(string prefix);
+    public TKey WithSuffix(string suffix);
+    public TKey WithKey(string key);
+}
