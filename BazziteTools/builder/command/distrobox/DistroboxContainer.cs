8f19f39f7dff4d5130822a32196a76ff0f5c7a48
andreas <Andreas.Zwander@gmail.com>
2026-02-06 18:19:11 +0100

    Add `DistroboxContainer` class for representing distrobox container details in `BazziteTools.builder.command.distrobox` namespace

---
+++ BazziteTools/builder/command/distrobox/DistroboxContainer.cs
@@ -0,0 +1,9 @@
+namespace BazziteTools.builder.command.distrobox;
+
+public class DistroboxContainer
+{
+    public string ID { get; set; }
+    public string Name { get; set; }
+    public string Status { get; set; }
+    public string Image { get; set; }
+}
