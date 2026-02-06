b8b1165c7617f1db7149147f9ab581875f20b868
andreas <Andreas.Zwander@gmail.com>
2026-02-06 15:22:40 +0100

    Add `IKeyValueParameter` interface to define key-value parameter behavior in `BazziteTools.builder.command.@base` namespace

---
+++ BazziteTools/builder/command/base/IKeyValueParameter.cs
@@ -0,0 +1,9 @@
+namespace BazziteTools.builder.command.@base;
+
+public interface IKeyValueParameter<TKey, out TValue> : IKeyParameter<TKey>
+{
+    string Value { get; set; }
+    string Separator { get; set; }
+    public TValue WithValue(string value);
+    public TValue WithSeparator(string separator);
+}
