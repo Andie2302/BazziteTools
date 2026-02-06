9783756d63acdff68ed1d97876406ad579b6c5ae
andreas <Andreas.Zwander@gmail.com>
2026-02-06 18:27:48 +0100

    Move `ShortOptionParameter` to `BazziteTools.builder.command.@base.special` namespace and add dependencies on `@base.enums` and `@base.interfaces`

--- BazziteTools/builder/command/base/ShortOptionParameter.cs
+++
@@ -1,3 +0,0 @@
-namespace BazziteTools.builder.command.@base;
-
-public class ShortOptionParameter : KeyValueParameter { public ShortOptionParameter() { Prefix = Prefixes.Dash.ToValue(); Separator = " "; } }
---
+++ BazziteTools/builder/command/base/special/ShortOptionParameter.cs
@@ -0,0 +1,6 @@
+using BazziteTools.builder.command.@base.enums;
+using BazziteTools.builder.command.@base.interfaces;
+
+namespace BazziteTools.builder.command.@base.special;
+
+public class ShortOptionParameter : KeyValueParameter { public ShortOptionParameter() { Prefix = Prefixes.Dash.ToValue(); Separator = " "; } }
