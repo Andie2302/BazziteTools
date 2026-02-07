06c426044f79903cddbc14150244f670371d7fb6
andreas <Andreas.Zwander@gmail.com>
2026-02-07 19:12:40 +0100

    Refactor `LongOptionParameter` to inherit from `KeyParameter` and use `enums.Prefixes.DoubleDash` for prefix

--- BazziteTools/builder/command/base/special/LongOptionParameter.cs
+++ BazziteTools/builder/command/base/special/LongOptionParameter.cs
@@ -1,13 +1,10 @@
-using BazziteTools.builder.command.@base.enums;
-using BazziteTools.builder.command.@base.interfaces;
 
 namespace BazziteTools.builder.command.@base.special;
 
-public class LongOptionParameter : KeyValueParameter<LongOptionParameter, string>
+public class LongOptionParameter : KeyParameter<LongOptionParameter>
 {
     public LongOptionParameter()
     {
-        Prefix = Prefixes.DoubleDash;
-        Separator = " ";
+        Prefix = enums.Prefixes.DoubleDash;
     }
 }
