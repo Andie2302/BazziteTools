dfbdee1337ac02711b46fbbe1e2b04ed7f816c65
andreas <Andreas.Zwander@gmail.com>
2026-02-07 19:04:21 +0100

    Remove comment and adjust whitespace in `LongOptionParameter` constructor

--- BazziteTools/builder/command/base/special/LongOptionParameter.cs
+++ BazziteTools/builder/command/base/special/LongOptionParameter.cs
@@ -3,12 +3,11 @@ using BazziteTools.builder.command.@base.interfaces;
 
 namespace BazziteTools.builder.command.@base.special;
 
-// Hier geben wir 'LongOptionParameter' als T an die Basisklasse weiter
 public class LongOptionParameter : KeyValueParameter<LongOptionParameter, string>
 {
     public LongOptionParameter()
     {
         Prefix = Prefixes.DoubleDash;
-        Separator = " "; // Standard für Long Options: --key value
+        Separator = " ";
     }
 }
