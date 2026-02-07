0bf5dda340e222f9793f0478be137f5870c9458a
andreas <Andreas.Zwander@gmail.com>
2026-02-07 19:04:16 +0100

    Refactor `LongFlagParameter` to use generic base class and adjust constructor for `DoubleDash` prefix

--- BazziteTools/builder/command/base/special/LongFlagParameter.cs
+++ BazziteTools/builder/command/base/special/LongFlagParameter.cs
@@ -2,4 +2,10 @@ using BazziteTools.builder.command.@base.enums;
 
 namespace BazziteTools.builder.command.@base.special;
 
-public class LongFlagParameter : KeyParameter { public LongFlagParameter() => Prefix = Prefixes.DoubleDash; }
+public class LongFlagParameter : KeyParameter<LongFlagParameter>
+{
+    public LongFlagParameter()
+    {
+        Prefix = Prefixes.DoubleDash;
+    }
+}
