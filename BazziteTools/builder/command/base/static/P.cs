befcde4dba79bdb8233e01765705fbb67711d02c
andreas <Andreas.Zwander@gmail.com>
2026-02-07 19:00:16 +0100

    Refactor static methods in `P` class to improve parameter handling and method chaining

--- BazziteTools/builder/command/base/static/P.cs
+++ BazziteTools/builder/command/base/static/P.cs
@@ -1,18 +1,36 @@
-using BazziteTools.builder.command.@base.interfaces;
 using BazziteTools.builder.command.@base.special;
+using BazziteTools.builder.command.@base.interfaces;
 
 namespace BazziteTools.builder.command.@base.@static;
 
 public static class P
 {
-    public static LongFlagParameter LFlag(string key) => new() { Key = key };
-    public static ShortFlagParameter Flag(string key) => new() { Key = key };
-    
-    public static LongOptionParameter LOpt(string key, string value) => new() { Key = key, Value = value };
-    public static ShortOptionParameter Opt(string key, string value) => new() { Key = key, Value = value };
+    public static LongOptionParameter LongOption(string key = "") 
+        => new LongOptionParameter().WithKey(key);
 
-    public static KeyValueParameter Assign(string key, string value) => new() { Key = key, Value = value };
+    public static ShortFlagParameter ShortFlag(string key = "") 
+        => new ShortFlagParameter().WithKey(key);
     
-    public static KeyParameter Verb(string name) => new ArgumentParameter(name);
-    public static ArgumentParameter Arg(string value) => new(value);
+    public static LongFlagParameter LFlag(string key = "") 
+        => new LongFlagParameter().WithKey(key);
+
+    public static ShortFlagParameter Flag(string key = "") 
+        => new ShortFlagParameter().WithKey(key);
+
+    public static LongOptionParameter LOpt(string key = "", string value = "") 
+        => new LongOptionParameter().WithKey(key).WithValue(value);
+
+    public static ShortOptionParameter Opt(string key = "", string value = "") 
+        => new ShortOptionParameter().WithKey(key).WithValue(value);
+
+    public static KeyValueParameter<KeyValueParameter<dynamic, string>, string> Assign(string key = "", string value = "") 
+        => new KeyValueParameter<KeyValueParameter<dynamic, string>, string>().WithKey(key).WithValue(value);
+
+    public static KeyParameter Verb(string name = "") 
+        => new KeyParameter().WithKey(name);
+
+    public static ArgumentParameter Arg(string value) 
+        => new ArgumentParameter(value);
 }
+
+   
