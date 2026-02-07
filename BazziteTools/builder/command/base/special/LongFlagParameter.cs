43f75cdf8b6d6f0fad8b17adbf02ce2aad30eb03
andreas <Andreas.Zwander@gmail.com>
2026-02-07 19:12:35 +0100

    Refactor `LongFlagParameter` constructor to use `enums.Prefixes.DoubleDash` instead of direct reference to `Prefixes`

--- BazziteTools/builder/command/base/special/LongFlagParameter.cs
+++ BazziteTools/builder/command/base/special/LongFlagParameter.cs
@@ -1,11 +1,9 @@
-using BazziteTools.builder.command.@base.enums;
-
 namespace BazziteTools.builder.command.@base.special;
 
 public class LongFlagParameter : KeyParameter<LongFlagParameter>
 {
     public LongFlagParameter()
     {
-        Prefix = Prefixes.DoubleDash;
+        Prefix = enums.Prefixes.DoubleDash;
     }
 }
