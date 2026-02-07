e8a6c2157730716d1062c93dbfce713817e21df0
andreas <Andreas.Zwander@gmail.com>
2026-02-07 19:18:18 +0100

    Add `UseSudo` property to `Command` class and update `Build` method to prepend `sudo` when enabled

--- BazziteTools/builder/command/base/Command.cs
+++ BazziteTools/builder/command/base/Command.cs
@@ -1,17 +1,38 @@
+using System.Text;
 using BazziteTools.builder.command.@base.interfaces;
+
 namespace BazziteTools.builder.command.@base;
+
 public class Command(string executableName)
 {
     private readonly List<ICommandParameter> _parameters = [];
+    public bool UseSudo { get; set; }
+
     public Command Add(params ICommandParameter[] parameters)
     {
         _parameters.AddRange(parameters);
         return this;
     }
+
     public string Build()
     {
-        var rendered = _parameters.Select(p => p.Build());
-        return $"{executableName} {string.Join(" ", rendered)}".Trim();
+        var sb = new StringBuilder();
+
+        if (UseSudo)
+        {
+            sb.Append("sudo ");
+        }
+
+        sb.Append(executableName);
+
+        foreach (var parameter in _parameters)
+        {
+            sb.Append(' ');
+            sb.Append(parameter.Build());
+        }
+
+        return sb.ToString();
     }
+
     public override string ToString() => Build();
 }
