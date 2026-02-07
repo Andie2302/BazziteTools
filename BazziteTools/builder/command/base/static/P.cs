99e05d869d9940da30a8f1327766eee67d466885
andreas <Andreas.Zwander@gmail.com>
2026-02-07 18:54:33 +0100

    Refactor static factory methods in `P` class to use target-typed `new()` instantiation.

--- BazziteTools/builder/command/base/static/P.cs
+++ BazziteTools/builder/command/base/static/P.cs
@@ -5,13 +5,13 @@ namespace BazziteTools.builder.command.@base.@static;
 
 public static class P
 {
-    public static LongFlagParameter LFlag(string key) => new LongFlagParameter { Key = key };
-    public static ShortFlagParameter Flag(string key) => new ShortFlagParameter { Key = key };
+    public static LongFlagParameter LFlag(string key) => new() { Key = key };
+    public static ShortFlagParameter Flag(string key) => new() { Key = key };
     
-    public static LongOptionParameter LOpt(string key, string value) => new LongOptionParameter { Key = key, Value = value };
-    public static ShortOptionParameter Opt(string key, string value) => new ShortOptionParameter { Key = key, Value = value };
+    public static LongOptionParameter LOpt(string key, string value) => new() { Key = key, Value = value };
+    public static ShortOptionParameter Opt(string key, string value) => new() { Key = key, Value = value };
 
-    public static KeyValueParameter Assign(string key, string value) => new KeyValueParameter { Key = key, Value = value };
+    public static KeyValueParameter Assign(string key, string value) => new() { Key = key, Value = value };
     
     public static KeyParameter Verb(string name) => new ArgumentParameter(name);
     public static ArgumentParameter Arg(string value) => new(value);
