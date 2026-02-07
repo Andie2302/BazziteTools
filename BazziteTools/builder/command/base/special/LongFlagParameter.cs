7ee33b485685cfcca3c77336ae5022e501038a91
andreas <Andreas.Zwander@gmail.com>
2026-02-07 18:41:55 +0100

    Update `LongFlagParameter` to use `Prefixes` enum without `.ToValue()` method

--- BazziteTools/builder/command/base/special/LongFlagParameter.cs
+++ BazziteTools/builder/command/base/special/LongFlagParameter.cs
@@ -2,4 +2,4 @@ using BazziteTools.builder.command.@base.enums;
 
 namespace BazziteTools.builder.command.@base.special;
 
-public class LongFlagParameter : KeyParameter { public LongFlagParameter() => Prefix = Prefixes.DoubleDash.ToValue(); }
+public class LongFlagParameter : KeyParameter { public LongFlagParameter() => Prefix = Prefixes.DoubleDash; }
