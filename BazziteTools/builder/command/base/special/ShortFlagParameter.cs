7068f454a50a0293bc87769349de625c89194096
andreas <Andreas.Zwander@gmail.com>
2026-02-07 18:42:06 +0100

    Update `ShortFlagParameter` to use `Prefixes` enum without `.ToValue()` method

--- BazziteTools/builder/command/base/special/ShortFlagParameter.cs
+++ BazziteTools/builder/command/base/special/ShortFlagParameter.cs
@@ -2,4 +2,4 @@ using BazziteTools.builder.command.@base.enums;
 
 namespace BazziteTools.builder.command.@base.special;
 
-public class ShortFlagParameter : KeyParameter { public ShortFlagParameter() => Prefix = Prefixes.Dash.ToValue(); }
+public class ShortFlagParameter : KeyParameter { public ShortFlagParameter() => Prefix = Prefixes.Dash; }
