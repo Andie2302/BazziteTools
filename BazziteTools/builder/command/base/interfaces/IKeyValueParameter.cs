8d7660c4b29154834de33c0879a32ab3cd58aac2
andreas <Andreas.Zwander@gmail.com>
2026-02-07 19:12:13 +0100

    Remove comment in `IKeyValueParameter` interface file

--- BazziteTools/builder/command/base/interfaces/IKeyValueParameter.cs
+++ BazziteTools/builder/command/base/interfaces/IKeyValueParameter.cs
@@ -1,6 +1,5 @@
 namespace BazziteTools.builder.command.@base.interfaces;
 
-// IKeyValueParameter.cs
 public interface IKeyValueParameter<out T, TValue> : IKeyParameter<T> where T : IKeyValueParameter<T, TValue>
 {
     TValue Value { get; set; }
