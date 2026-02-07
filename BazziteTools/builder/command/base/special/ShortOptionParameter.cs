9ca714a5d74b60feb2f8f5815f048963a8eb31a5
andreas <Andreas.Zwander@gmail.com>
2026-02-07 19:04:31 +0100

    Update `ShortOptionParameter` to use `Dash` prefix instead of `DoubleDash`

--- BazziteTools/builder/command/base/special/ShortOptionParameter.cs
+++ BazziteTools/builder/command/base/special/ShortOptionParameter.cs
@@ -3,12 +3,11 @@ using BazziteTools.builder.command.@base.interfaces;
 
 namespace BazziteTools.builder.command.@base.special;
 
-// Hier geben wir 'LongOptionParameter' als T an die Basisklasse weiter
 public class ShortOptionParameter : KeyValueParameter<ShortOptionParameter, string>
 {
     public ShortOptionParameter()
     {
-        Prefix = Prefixes.DoubleDash;
+        Prefix = Prefixes.Dash;
         Separator = " ";
     }
 }
