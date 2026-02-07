3f134946a68f57eb082b2426bce06e7c77cc2876
andreas <Andreas.Zwander@gmail.com>
2026-02-07 16:59:55 +0100

    Remove unused `_executable` field from `Command` class in `builder.command.base` namespace.

--- BazziteTools/builder/command/base/Command.cs
+++ BazziteTools/builder/command/base/Command.cs
@@ -2,7 +2,6 @@ using BazziteTools.builder.command.@base.interfaces;
 namespace BazziteTools.builder.command.@base;
 public class Command(string executableName)
 {
-    private readonly string _executable = executableName;
     private readonly List<ICommandParameter> _parameters = [];
     public Command Add(params ICommandParameter[] parameters)
     {
@@ -12,7 +11,7 @@ public class Command(string executableName)
     public string Build()
     {
         var rendered = _parameters.Select(p => p.Build());
-        return $"{_executable} {string.Join(" ", rendered)}".Trim();
+        return $"{executableName} {string.Join(" ", rendered)}".Trim();
     }
     public override string ToString() => Build();
 }
